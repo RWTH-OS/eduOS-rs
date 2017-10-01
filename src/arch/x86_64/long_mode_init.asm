79f7dd533bec5ca10a9b910abdcd65aedd27feb0
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2017-10-01 12:20:44 +0200

    add license, remove compiler warnings

--- src/arch/x86_64/long_mode_init.asm
+++ src/arch/x86_64/long_mode_init.asm
@@ -1,12 +1,31 @@
+; Copyright (c) 2017 Stefan Lankes, RWTH Aachen University
+;
+; MIT License
+;
+; Permission is hereby granted, free of charge, to any person obtaining
+; a copy of this software and associated documentation files (the
+; "Software"), to deal in the Software without restriction, including
+; without limitation the rights to use, copy, modify, merge, publish,
+; distribute, sublicense, and/or sell copies of the Software, and to
+; permit persons to whom the Software is furnished to do so, subject to
+; the following conditions:
+;
+; The above copyright notice and this permission notice shall be
+; included in all copies or substantial portions of the Software.
+;
+; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+; EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+; MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+; NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+; LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+; OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+; WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 ;;; Based on http://blog.phil-opp.com/rust-os/entering-longmode.html
 ;;; and http://blog.phil-opp.com/rust-os/setup-rust.html
 ;;;
 ;;; Once we've run all our 32-bit setup code, we jump here and enter 64-bit
 ;;; mode.
-;;;
-;;; To generate yellow 4-letter debug text values, you can run:
-;;;
-;;; "INT!".chars.map {|c| sprintf("2f%02x", c.ord) }.reverse.join
 
 %include 'common.inc'
 
@@ -17,47 +36,6 @@ extern rust_main
 section .text
 bits 64
 long_mode_start:
-        call setup_SSE
         call rust_main
 
-        ;; Display "OKAY".
-        mov rax, 0x2f592f412f4b2f4f
-        mov qword [SCREEN_BASE], rax
         hlt
-
-;;; Print "ERROR: " and an error code.
-;;;
-;;; a1: Error code.
-error:
-        mov rbx, 0x4f4f4f524f524f45
-        mov [SCREEN_BASE], rbx
-        mov rbx, 0x4f204f204f3a4f52
-        mov [SCREEN_BASE + 8], rbx
-        mov byte [SCREEN_BASE + 0xe], al
-        hlt
-
-;;; Check for SSE and enable it, or display an error.
-;;;
-;;; Copied from http://blog.phil-opp.com/rust-os/setup-rust.html, which got
-;;; it from http://wiki.osdev.org/SSE#Checking_for_SSE.
-setup_SSE:
-        ;; Check for SSE.
-        mov rax, 0x1
-        cpuid
-        test edx, 1<<25
-        jz .no_SSE
-
-        ;; Enable SSE.
-        mov rax, cr0
-        and ax, 0xFFFB      ; Clear coprocessor emulation CR0.EM.
-        or ax, 0x2          ; Set coprocessor monitoring  CR0.MP.
-        mov cr0, rax
-        mov rax, cr4
-        or ax, 3 << 9       ; Set CR4.OSFXSR and CR4.OSXMMEXCPT.
-        mov cr4, rax
-
-        ret
-.no_SSE:
-        mov al, "S"
-        jmp error
-
