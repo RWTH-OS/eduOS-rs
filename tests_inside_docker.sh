8e720c08c8005d10fa1ccabaa8335cc041dadbe5
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2017-10-03 21:22:21 +0200

    install automake within the container

--- tests_inside_docker.sh
+++ tests_inside_docker.sh
@@ -12,12 +12,12 @@ yum -y clean all
 yum -y clean expire-cache
 
 # First, install all the needed packages.
-yum install -y curl wget qemu-system-x86 nasm make
+yum install -y curl wget qemu-system-x86 nasm make automake
 
 elif [ "$OS_NAME" = "ubuntu" ]; then
 
 apt-get -qq update
-apt-get install -y curl wget qemu-system-x86 nasm make
+apt-get install -y curl wget qemu-system-x86 nasm make automake
 
 fi
 
