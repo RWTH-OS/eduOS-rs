7243a713852bb0ade3fd9015ebe28bc72f939fee
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2017-10-03 22:33:02 +0200

    add missing description of the OS

--- setup_tests.sh
+++ setup_tests.sh
@@ -18,7 +18,7 @@ elif [ "$OS_VERSION" = "7" ]; then
 docker run --privileged -d -ti -e "container=docker"  -v /sys/fs/cgroup:/sys/fs/cgroup -v `pwd`/..:/rwth-os:rw  ${OS_TYPE}:${OS_VERSION}   /usr/sbin/init
 DOCKER_CONTAINER_ID=$(docker ps | grep centos | awk '{print $1}')
 docker logs $DOCKER_CONTAINER_ID
-docker exec -ti $DOCKER_CONTAINER_ID /bin/bash -xec "bash -xe /rwth-os/eduOS-rs/tests_inside_docker.sh ${OS_VERSION};
+docker exec -ti $DOCKER_CONTAINER_ID /bin/bash -xec "bash -xe /rwth-os/eduOS-rs/tests_inside_docker.sh ${OS_TYPE} ${OS_VERSION};
   echo -ne \"------\nEND A SHORT TESTS\n\";"
 docker ps -a
 docker stop $DOCKER_CONTAINER_ID
