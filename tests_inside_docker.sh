13705dac700f2965b99d06ecb3333f4b01accf60
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2017-10-03 16:51:37 +0200

    move tests to a docker container

---
+++ tests_inside_docker.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+OS_NAME=$1
+OS_VERSION=$2
+
+cd /rwth-os/eduOS-rs/
+
+if [ "$OS_NAME" = "centos" ]; then
+
+# Clean the yum cache
+yum -y clean all
+yum -y clean expire-cache
+
+# First, install all the needed packages.
+yum install -y curl wget qemu-system-x86 nasm texinfo flex bison
+
+elif [ "$OS_NAME" = "ubuntu" ]; then
+
+apt-get -qq update
+apt-get install -y curl wget qemu-system-x86 nasm texinfo
+
+fi
+
+curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain nightly
+export PATH=$PATH:~/.cargo/bin
+make runtime
+make
+make run
