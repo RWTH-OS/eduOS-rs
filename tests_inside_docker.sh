abe84f3a24fa8e03edcaccf3f60786ce9ce68013
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2017-11-10 08:37:34 +0100

    install also 32bit version of glibc

--- tests_inside_docker.sh
+++ tests_inside_docker.sh
@@ -12,21 +12,23 @@ yum -y clean all
 yum -y clean expire-cache
 
 # First, install all the needed packages.
-yum install -y curl wget qemu-system-x86 nasm make autotools gcc gcc-c++
+yum install -y curl wget qemu-system-x86 nasm make autotools gcc gcc-c++ glibc-devel.i686
 
 elif [ "$OS_NAME" = "ubuntu" ]; then
 
 apt-get -qq update
-apt-get install -y curl wget qemu-system-x86 nasm make autotools-dev gcc g++ build-essential
+apt-get install -y curl wget qemu-system-x86 nasm make autotools-dev gcc g++ build-essential g++-multilib
 
 fi
 
 curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain nightly
 export PATH=$PATH:~/.cargo/bin
+
 # tests in 64bit mode
 make runtime
 make
 make run
+
 # tests in 32bit mode
 make arch=i686 runtime
 make arch=i686
