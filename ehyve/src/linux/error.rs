10f8aabae6e52fdb9d3a2e98b071310153b3fc02
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2018-09-04 13:12:59 +0200

    increasing the readability

--- ehyve/src/linux/error.rs
+++ ehyve/src/linux/error.rs
@@ -5,9 +5,9 @@ pub type Result<T> = result::Result<T, Error>;
 
 #[derive(Debug, Clone)]
 pub enum Error {
-    InternalError,
-    NotEnoughMemory,
-    InvalidFile(String),
+	InternalError,
+	NotEnoughMemory,
+	InvalidFile(String),
 	UnknownExitReason(::linux::kvm::Exit),
 	KVMInitFailed,
 	KVMUnableToCreateVM,
@@ -16,16 +16,16 @@ pub enum Error {
 }
 
 impl fmt::Display for Error {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match *self {
-            Error::InternalError => write!(f, "An internal error has occurred, please report."),
-            Error::NotEnoughMemory => write!(f, "The host system has not enough memory, please check your memory usage."),
-            Error::InvalidFile(ref file) => write!(f, "The file {} was not found or is invalid.", file),
+	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+		match *self {
+			Error::InternalError => write!(f, "An internal error has occurred, please report."),
+			Error::NotEnoughMemory => write!(f, "The host system has not enough memory, please check your memory usage."),
+			Error::InvalidFile(ref file) => write!(f, "The file {} was not found or is invalid.", file),
 			Error::UnknownExitReason(ref exit_reason) => write!(f, "Unknowm exit reason {:?}.", exit_reason),
 			Error::KVMInitFailed => write!(f, "Unable to initialize KVM"),
 			Error::KVMUnableToCreateVM => write!(f, "Unable to create VM"),
 			Error::MissingFrequency => write!(f, "Couldn't get the CPU frequency from your system. (is /proc/cpuinfo missing?)"),
 			Error::ParseMemory => write!(f, "Couldn't parse the guest memory size from the environment")
-        }
-    }
+		}
+	}
 }
--- ehyve/src/macos/error.rs
+++ ehyve/src/macos/error.rs
@@ -5,11 +5,11 @@ pub type Result<T> = result::Result<T, Error>;
 
 #[derive(Debug,Clone)]
 pub enum Error {
-    FileMissing,
-    InternalError,
+	FileMissing,
+	InternalError,
 	InvalidFile(String),
-    NotEnoughMemory,
-    MissingFrequency,
+	NotEnoughMemory,
+	MissingFrequency,
 	Hypervisor(hypervisor::Error),
 	ParseMemory,
 	UnhandledExitReason
@@ -24,16 +24,16 @@ pub fn to_error(err: hypervisor::Error) -> Result<()>
 }
 
 impl fmt::Display for Error {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match *self {
-            Error::FileMissing => write!(f, "No execution file given"),
-            Error::InternalError => write!(f, "An internal error has occurred, please report."),
+	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+		match *self {
+			Error::FileMissing => write!(f, "No execution file given"),
+			Error::InternalError => write!(f, "An internal error has occurred, please report."),
 			Error::InvalidFile(ref file) => write!(f, "The file {} was not found or is invalid.", file),
-            Error::NotEnoughMemory => write!(f, "The host system has not enough memory, please check your memory usage."),
-            Error::MissingFrequency => write!(f, "Couldn't get the CPU frequency from your system. (is /proc/cpuinfo missing?)"),
+			Error::NotEnoughMemory => write!(f, "The host system has not enough memory, please check your memory usage."),
+			Error::MissingFrequency => write!(f, "Couldn't get the CPU frequency from your system. (is /proc/cpuinfo missing?)"),
 			Error::Hypervisor(ref err) => write!(f, "The hypervisor has failed: {:?}", err),
 			Error::ParseMemory => write!(f, "Couldn't parse the guest memory size from the environment"),
 			Error::UnhandledExitReason => write!(f, "Unhandled exit reason")
-        }
-    }
+		}
+	}
 }
