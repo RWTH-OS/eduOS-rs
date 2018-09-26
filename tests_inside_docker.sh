c9550aaaf175a61a0e962db7d05ca79dd70cfd91
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2018-09-26 23:02:32 +0200

    disable final kernel test

--- tests_inside_docker.sh
+++ tests_inside_docker.sh
@@ -21,7 +21,7 @@ export PATH=$PATH:~/.cargo/bin
 cargo install cargo-xbuild bootimage
 rustup component add rust-src
 make bootimage.bin
-qemu-kvm -display none -smp 1 -device isa-debug-exit,iobase=0xf4,iosize=0x04 -serial stdio -drive format=raw,file=bootimage.bin
+#qemu-kvm -display none -smp 1 -device isa-debug-exit,iobase=0xf4,iosize=0x04 -serial stdio -drive format=raw,file=bootimage.bin
 
 elif [ "$OS_NAME" = "ubuntu" ]; then
 
@@ -32,7 +32,7 @@ curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain nightly
 export PATH=$PATH:~/.cargo/bin
 cargo install cargo-xbuild bootimage
 rustup component add rust-src
-make
-make qemu
+make bootimage.bin
+#make qemu
 
 fi
