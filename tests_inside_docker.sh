82fe06044a501fdf1a574453598dc74fe005b2a4
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2018-09-26 22:20:13 +0200

    fix name of qemu in the test scripts

--- tests_inside_docker.sh
+++ tests_inside_docker.sh
@@ -7,19 +7,26 @@ cd /rwth-os/eduOS-rs/
 
 if [ "$OS_NAME" = "centos" ]; then
 
+export PATH=/usr/libexec:$PATH
+
 # Clean the yum cache
 yum -y clean all
 yum -y clean expire-cache
 
 # First, install all the needed packages.
-yum install -y curl wget qemu-system-x86 nasm make autotools gcc gcc-c++
+yum install -y curl wget nasm make qemu-kvm gcc gcc-c++
+
+curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain nightly
+export PATH=$PATH:~/.cargo/bin
+cargo install cargo-xbuild bootimage
+rustup component add rust-src
+make bootimage
+qemu-kvm -display none -smp 1 -device isa-debug-exit,iobase=0xf4,iosize=0x04 -serial stdio -drive format=raw,file=bootimage.bin
 
 elif [ "$OS_NAME" = "ubuntu" ]; then
 
 apt-get -qq update
-apt-get install -y curl wget qemu-system-x86 nasm make autotools-dev gcc g++ build-essential
-
-fi
+apt-get install -y curl wget qemu-system-x86 nasm make gcc g++ build-essential
 
 curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain nightly
 export PATH=$PATH:~/.cargo/bin
@@ -27,3 +34,5 @@ cargo install cargo-xbuild bootimage
 rustup component add rust-src
 make
 make qemu
+
+fi
