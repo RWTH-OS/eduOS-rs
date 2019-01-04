cb1b18910ea1ec4343e7c9fce152aa3770c7752c
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2019-01-04 12:28:28 +0100

    remove dependency from cpuio

--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,6 @@
 
 extern crate spin;
 #[cfg(target_arch = "x86_64")]
-extern crate cpuio;
-#[cfg(target_arch = "x86_64")]
 extern crate x86;
 
 // These need to be visible to the linker, so we need to export them.
