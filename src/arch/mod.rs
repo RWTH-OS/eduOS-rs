033f22bc3980b146c69d9e8904445138ed69132b
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2019-12-20 22:59:34 +0100

    remove wasm support

--- src/arch/mod.rs
+++ src/arch/mod.rs
@@ -25,19 +25,12 @@
 #[cfg(target_arch = "x86_64")]
 pub use self::x86_64::{processor, serial};
 
-#[cfg(target_arch = "wasm32")]
-pub use self::wasm32::serial;
-
 #[cfg(target_arch = "aarch64")]
 pub use self::aarch64::{processor, serial};
 
 // Implementations for x86_64.
 #[cfg(target_arch = "x86_64")]
 pub mod x86_64;
-
-// Implementations for wasm32.
-#[cfg(target_arch = "wasm32")]
-pub mod wasm32;
 
 // Implementations for aarch64.
 #[cfg(target_arch = "aarch64")]
