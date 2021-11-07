8c07368fd3ccb82294e7c5bad720dbb237c2415e
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2021-11-07 08:36:24 +0100

    remove debug messages

--- src/arch/x86_64/processor.rs
+++ src/arch/x86_64/processor.rs
@@ -28,7 +28,6 @@ pub fn mb() {
 /// Search the most significant bit
 #[inline(always)]
 pub fn msb(value: u64) -> Option<u64> {
-	println!("value {}", value);
 	if value > 0 {
 		let ret: u64;
 		unsafe {
@@ -38,7 +37,6 @@ pub fn msb(value: u64) -> Option<u64> {
 				options(nomem, nostack)
 			);
 		}
-		println!("value {} {}", value, ret);
 		Some(ret)
 	} else {
 		None
