b8c8eb43876194c716902cdde6a46070838b4255
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2021-12-08 22:59:20 +0100

    switch to latest toolchain

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
 #![feature(const_ptr_offset, asm, lang_items)]
+#![feature(asm_const)]
+#![feature(asm_sym)]
 #![feature(allocator_api)]
 #![feature(panic_info_message)]
 #![feature(naked_functions)]
