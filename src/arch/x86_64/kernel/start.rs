84b8c633309aeef67e2db9c8795f86d182c86c3d
Stefan Lankes <stlankes@users.noreply.github.com>
2022-11-13 18:33:09 +0100

    using bootimage to build a bootable disk image (#25)
    * using bootimage to build a bootable disk image
    
    * using qemu-exit to leave the kernel
    
    * revise readme to describe the usage of Qemu

--- src/arch/x86_64/kernel/start.rs
+++ src/arch/x86_64/kernel/start.rs
@@ -5,33 +5,14 @@
 // http://opensource.org/licenses/MIT>, at your option. This file may not be
 // copied, modified, or distributed except according to those terms.
 
-use crate::consts::STACK_SIZE;
-use crate::scheduler::task::BOOT_STACK;
-use core::arch::asm;
-
 extern "C" {
 	pub fn main();
 }
 
 #[cfg(not(test))]
 #[no_mangle]
-pub unsafe extern "C" fn pre_main() -> ! {
+pub unsafe extern "C" fn _start() -> ! {
 	main();
 
 	loop {}
-}
-
-#[cfg(not(test))]
-#[no_mangle]
-#[naked]
-pub unsafe extern "C" fn _start() -> ! {
-	asm!(
-		// initialize stack pointer
-		"lea rsp, [{stack}+{size}]",
-		"call {pre_main}",
-		stack = sym BOOT_STACK,
-		size = const STACK_SIZE - 16,
-		pre_main = sym pre_main,
-		options(noreturn)
-	);
 }
