9fa4553f010148c830e637428ac0a911381125c7
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2022-11-13 11:24:51 +0100

    use qemu-exit to leave the kernel

--- src/arch/x86_64/processor.rs
+++ src/arch/x86_64/processor.rs
@@ -2,7 +2,7 @@
 
 use core::arch::asm;
 use x86::controlregs::*;
-use x86::io::*;
+use qemu_exit::QEMUExit;
 
 pub fn halt() {
 	unsafe {
@@ -13,13 +13,8 @@ pub fn halt() {
 #[no_mangle]
 pub extern "C" fn shutdown() -> ! {
 	// shutdown, works like Qemu's shutdown command
-	unsafe {
-		outb(0xf4, 0x00);
-	}
-
-	loop {
-		halt();
-	}
+	let qemu_exit_handle = qemu_exit::X86::new(0xf4, 5);
+	qemu_exit_handle.exit_success();
 }
 
 pub fn cpu_init() {
