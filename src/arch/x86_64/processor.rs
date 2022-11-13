8c8133dca820cdd688e177f996b0e488d7bb8878
Stefan Lankes <stlankes@users.noreply.github.com>
2022-11-13 18:33:09 +0100

    using bootimage to build a bootable disk image (#25)
    * using bootimage to build a bootable disk image
    
    * using qemu-exit to leave the kernel
    
    * revise readme to describe the usage of Qemu

--- src/arch/x86_64/processor.rs
+++ src/arch/x86_64/processor.rs
@@ -1,8 +1,8 @@
 #![allow(dead_code)]
 
 use core::arch::asm;
+use qemu_exit::QEMUExit;
 use x86::controlregs::*;
-use x86::io::*;
 
 pub fn halt() {
 	unsafe {
@@ -13,13 +13,8 @@ pub fn halt() {
 #[no_mangle]
 pub extern "C" fn shutdown() -> ! {
 	// shutdown, works like Qemu's shutdown command
-	unsafe {
-		outb(0xf4, 0x00);
-	}
-
-	loop {
-		halt();
-	}
+	let qemu_exit_handle = qemu_exit::X86::new(0xf4, 5);
+	qemu_exit_handle.exit_success();
 }
 
 pub fn cpu_init() {
