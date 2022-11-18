847bf00459f2f5f6d69206f0c646a966993e3714
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2022-11-18 22:58:56 +0100

    reuse boot stack from bootloader
    - separate kernel and memory management code

---
+++ src/arch/x86_64/kernel/mod.rs
@@ -0,0 +1,10 @@
+pub mod irq;
+pub mod processor;
+pub mod serial;
+mod start;
+pub mod switch;
+pub mod task;
+
+use bootloader::BootInfo;
+
+pub(crate) static mut BOOT_INFO: Option<&'static BootInfo> = None;
