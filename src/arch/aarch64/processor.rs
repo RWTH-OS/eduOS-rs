51a236e11b94e8bfe8528da9763118226e946ff7
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2024-05-01 19:27:49 +0200

    convert assembly to the new inline format

--- src/arch/aarch64/processor.rs
+++ src/arch/aarch64/processor.rs
@@ -1,13 +1,15 @@
 #![allow(dead_code)]
+use core::arch::asm;
 
 #[no_mangle]
 pub extern "C" fn shutdown() -> ! {
 	unsafe {
-		asm!("mov w0, 0x18\n\t
-			mov x1, #0x20000\n\t
-			add x1, x1, #0x26\n\t
-			hlt #0xF000" :::: "volatile");
+		asm!(
+			"mov w0, 0x18",
+			"mov x1, #0x20000",
+			"add x1, x1, #0x26",
+			"hlt #0xF000",
+			options(nostack, nomem, noreturn),
+		);
 	}
-
-	loop {}
 }
--- src/arch/aarch64/start.rs
+++ src/arch/aarch64/start.rs
@@ -1,12 +1,7 @@
-// Copyright (c) 2019 Stefan Lankes, RWTH Aachen University
-//
-// Licensed under the Apache License, Version 2.0, <LICENSE-APACHE or
-// http://apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT or
-// http://opensource.org/licenses/MIT>, at your option. This file may not be
-// copied, modified, or distributed except according to those terms.
+use core::arch::asm;
 
 extern "C" {
-	fn main();
+	fn main() -> !;
 }
 
 #[cfg(not(test))]
@@ -14,10 +9,12 @@ extern "C" {
 #[naked]
 pub unsafe extern "C" fn _start() -> ! {
 	// init 1G stack stack in RAM area (>0x40000000 for qemu virt device)
-	asm!("movz x1, 0x5000, lsl 16\n\t
-        mov sp, x1" :::: "volatile");
-
-	main();
-
-	loop {}
+	asm!(
+		"movz x1, 0x5000, lsl 16",
+		"mov sp, x1",
+		// Jump to Rust code
+		"b {main}",
+		main = sym main,
+		options(noreturn),
+	);
 }
