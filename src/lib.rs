4a2d5497633ffacee24cc559a8138bdbc4db5b76
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2024-05-01 19:28:23 +0200

    remove obsolte definition external crates

--- src/lib.rs
+++ src/lib.rs
@@ -4,10 +4,6 @@
 #![no_std]
 #![no_main]
 
-extern crate spin;
-#[cfg(target_arch = "x86_64")]
-extern crate x86;
-
 // These need to be visible to the linker, so we need to export them.
 #[cfg(target_arch = "x86_64")]
 pub use arch::processor::*;
