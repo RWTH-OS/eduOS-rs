707b8e41f9e1d72e88dfc34c2dfed4a7ea0b8337
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2024-09-15 07:50:00 +0200

    rename architecture specific directory

--- src/arch/x86/kernel/gdt.rs
+++ src/arch/x86/kernel/gdt.rs
@@ -1,12 +1,3 @@
-// Copyright (c) 2017 Stefan Lankes, RWTH Aachen University
-//
-// Licensed under the Apache License, Version 2.0, <LICENSE-APACHE or
-// http://apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT or
-// http://opensource.org/licenses/MIT>, at your option. This file may not be
-// copied, modified, or distributed except according to those terms.
-
-#![allow(dead_code)]
-
 use crate::arch::mm::VirtAddr;
 use crate::consts::*;
 use core::mem;
@@ -15,7 +6,6 @@ use x86::bits64::task::*;
 use x86::dtables::{self, DescriptorTablePointer};
 use x86::segmentation::*;
 use x86::Ring;
-//use logging::*;
 use crate::scheduler;
 
 const GDT_NULL: usize = 0;
