1e224c5d10bd47ba05613c9fc3432c2730c8ee5b
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2024-09-15 08:49:47 +0200

    switch to the latest rust toolchain
    - use qemu_exit to return error code

--- src/arch/x86/kernel/processor.rs
+++ src/arch/x86/kernel/processor.rs
@@ -18,6 +18,7 @@ pub fn mb() {
 pub fn msb(value: u64) -> Option<u64> {
 	if value > 0 {
 		let ret: u64;
+
 		unsafe {
 			asm!("bsr {0}, {1}",
 				out(reg) ret,
@@ -49,28 +50,45 @@ pub fn lsb(value: u64) -> Option<u64> {
 	}
 }
 
-pub fn halt() {
+pub(crate) fn halt() {
 	unsafe {
 		asm!("hlt", options(nomem, nostack));
 	}
 }
 
-pub fn pause() {
+pub(crate) fn pause() {
 	unsafe {
 		asm!("pause", options(nomem, nostack));
 	}
 }
 
+#[allow(unused_variables)]
 #[no_mangle]
-pub extern "C" fn shutdown() -> ! {
-	// shutdown, works like Qemu's shutdown command
-	let qemu_exit_handle = qemu_exit::X86::new(0xf4, 5);
-	qemu_exit_handle.exit_success();
+pub extern "C" fn shutdown(error_code: i32) -> ! {
+	#[cfg(feature = "qemu-exit")]
+	{
+		let code = if error_code == 0 { 5 } else { 1 };
+
+		// shutdown, works like Qemu's shutdown command
+		let qemu_exit_handle = qemu_exit::X86::new(0xf4, code);
+		qemu_exit_handle.exit_success();
+	}
+
+	#[cfg(not(feature = "qemu-exit"))]
+	loop {
+		unsafe {
+			x86::halt();
+		}
+	}
 }
 
+<<<<<<< HEAD
 pub fn init() {
 	debug!("enable supported processor features");
 
+=======
+pub(crate) fn cpu_init() {
+>>>>>>> dd0e01d (switch to the latest rust toolchain)
 	let mut cr0 = unsafe { cr0() };
 
 	// be sure that AM, NE and MP is enabled
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-#![feature(panic_info_message)]
 #![feature(abi_x86_interrupt)]
 #![no_std] // don't link the Rust standard library
 #![cfg_attr(not(test), no_main)] // disable all Rust-level entry points
@@ -22,7 +21,7 @@ extern "C" fn foo() {
 /// named `_start` by default.
 #[cfg(not(test))]
 #[no_mangle] // don't mangle the name of this function
-pub extern "C" fn main() -> ! {
+pub extern "C" fn main() -> i32 {
 	arch::init();
 	scheduler::init();
 
@@ -39,6 +38,5 @@ pub extern "C" fn main() -> ! {
 
 	println!("Shutdown system!");
 
-	// shutdown system
-	shutdown();
+	0
 }
