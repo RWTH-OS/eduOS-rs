10c85f8f6a035f3ec2760be7f3a1c14fff3eabcf
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2024-09-15 08:49:47 +0200

    switch to the latest rust toolchain
    - use qemu_exit to return error code

--- src/arch/x86/kernel/processor.rs
+++ src/arch/x86/kernel/processor.rs
@@ -4,20 +4,33 @@ use core::arch::asm;
 use qemu_exit::QEMUExit;
 use x86::controlregs::*;
 
-pub fn halt() {
+pub(crate) fn halt() {
 	unsafe {
 		asm!("hlt", options(nomem, nostack));
 	}
 }
 
+#[allow(unused_variables)]
 #[no_mangle]
-pub extern "C" fn shutdown() -> ! {
-	// shutdown, works like Qemu's shutdown command
-	let qemu_exit_handle = qemu_exit::X86::new(0xf4, 5);
-	qemu_exit_handle.exit_success();
+pub extern "C" fn shutdown(error_code: i32) -> ! {
+	#[cfg(feature = "qemu-exit")]
+	{
+		let code = if error_code == 0 { 5 } else { 1 };
+
+		// shutdown, works like Qemu's shutdown command
+		let qemu_exit_handle = qemu_exit::X86::new(0xf4, code);
+		qemu_exit_handle.exit_success();
+	}
+
+	#[cfg(not(feature = "qemu-exit"))]
+	loop {
+		unsafe {
+			x86::halt();
+		}
+	}
 }
 
-pub fn cpu_init() {
+pub(crate) fn cpu_init() {
 	let mut cr0 = unsafe { cr0() };
 
 	// be sure that AM, NE and MP is enabled
