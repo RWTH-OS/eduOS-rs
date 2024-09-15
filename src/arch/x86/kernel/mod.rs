7d26d8920383a42406578561ea30a04b67f58a37
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2024-09-15 16:41:23 +0200

    add VGA support

--- src/arch/x86/kernel/mod.rs
+++ src/arch/x86/kernel/mod.rs
@@ -1,10 +1,13 @@
 pub mod irq;
 pub mod processor;
+#[cfg(not(all(target_arch = "x86", feature = "vga")))]
 pub mod serial;
 #[cfg(target_arch = "x86_64")]
 mod start;
 pub(crate) mod switch;
 pub(crate) mod task;
+#[cfg(all(target_arch = "x86", feature = "vga"))]
+pub mod vga;
 
 #[cfg(target_arch = "x86_64")]
 use bootloader::BootInfo;
@@ -13,3 +16,10 @@ pub(crate) static mut BOOT_INFO: Option<&'static BootInfo> = None;
 
 #[cfg(target_arch = "x86")]
 core::arch::global_asm!(include_str!("entry32.s"));
+
+pub fn init() {
+	processor::cpu_init();
+
+	#[cfg(all(target_arch = "x86", feature = "vga"))]
+	vga::init();
+}
