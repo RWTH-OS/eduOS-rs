fc3a19d18c72c80ca26175a7b19e4c11b670e217
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2024-09-15 16:22:22 +0200

    add basic 32bit support for x86

--- src/arch/x86/kernel/mod.rs
+++ src/arch/x86/kernel/mod.rs
@@ -3,13 +3,18 @@ pub mod irq;
 mod pit;
 pub mod processor;
 pub mod serial;
+#[cfg(target_arch = "x86_64")]
 mod start;
-pub mod switch;
-pub mod task;
+pub(crate) mod switch;
+pub(crate) mod task;
 
+#[cfg(target_arch = "x86_64")]
 use bootloader::BootInfo;
-
+#[cfg(target_arch = "x86_64")]
 pub(crate) static mut BOOT_INFO: Option<&'static BootInfo> = None;
+
+#[cfg(target_arch = "x86")]
+core::arch::global_asm!(include_str!("entry32.s"));
 
 /// Initialize module, must be called once, and only once
 pub fn init() {
--- src/arch/x86/kernel/task.rs
+++ src/arch/x86/kernel/task.rs
@@ -8,6 +8,7 @@ use crate::scheduler::{do_exit, get_current_taskid};
 use core::mem::size_of;
 use core::ptr::write_bytes;
 
+#[cfg(target_arch = "x86_64")]
 #[repr(C, packed)]
 struct State {
 	/// R15 register
@@ -48,6 +49,31 @@ struct State {
 	rip: u64,
 }
 
+#[cfg(target_arch = "x86")]
+#[repr(C, packed)]
+struct State {
+	/// EDI register
+	edi: u32,
+	/// ESI register
+	esi: u32,
+	/// EBP register
+	ebp: u32,
+	/// (pseudo) ESP register
+	esp: u32,
+	/// EBX register
+	ebx: u32,
+	/// EDX register
+	edx: u32,
+	/// ECX register
+	ecx: u32,
+	/// EAX register
+	eax: u32,
+	/// status flags
+	eflags: u32,
+	/// instruction pointer
+	eip: u32,
+}
+
 extern "C" fn leave_task() -> ! {
 	debug!("finish task {}", get_current_taskid());
 
@@ -59,6 +85,7 @@ extern "C" fn leave_task() -> ! {
 }
 
 impl TaskFrame for Task {
+	#[cfg(target_arch = "x86_64")]
 	fn create_stack_frame(&mut self, func: extern "C" fn()) {
 		unsafe {
 			let mut stack: *mut u64 = ((*self.stack).top()).as_mut_ptr();
@@ -85,6 +112,39 @@ impl TaskFrame for Task {
 
 			(*state).rip = (func as *const ()) as u64;
 			(*state).rflags = 0x1202u64;
+
+			/* Set the task's stack pointer entry to the stack we have crafted right now. */
+			self.last_stack_pointer = stack as usize;
+		}
+	}
+
+	#[cfg(target_arch = "x86")]
+	fn create_stack_frame(&mut self, func: extern "C" fn()) {
+		unsafe {
+			let mut stack: *mut u32 = ((*self.stack).top()).as_mut_ptr();
+
+			write_bytes((*self.stack).bottom().as_mut_ptr::<u8>(), 0xCD, STACK_SIZE);
+
+			/* Only marker for debugging purposes, ... */
+			*stack = 0xDEADBEEFu32;
+			stack = (stack as usize - size_of::<u32>()) as *mut u32;
+
+			/* the first-function-to-be-called's arguments, ... */
+			//TODO: add arguments
+
+			/* and the "caller" we shall return to.
+			 * This procedure cleans the task after exit. */
+			*stack = (leave_task as *const ()) as u32;
+			stack = (stack as usize - size_of::<State>()) as *mut u32;
+
+			let state: *mut State = stack as *mut State;
+			write_bytes(state, 0x00, 1);
+
+			(*state).esp = (stack as usize + size_of::<State>()) as u32;
+			(*state).ebp = (*state).esp + size_of::<u32>() as u32;
+
+			(*state).eip = (func as *const ()) as u32;
+			(*state).eflags = 0x1002u32;
 
 			/* Set the task's stack pointer entry to the stack we have crafted right now. */
 			self.last_stack_pointer = stack as usize;
