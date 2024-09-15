1e09ae0c06b5402315928386bfdcf3cf2c8a16cc
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2024-09-15 07:50:00 +0200

    rename architecture specific directory

--- src/arch/mod.rs
+++ src/arch/mod.rs
@@ -1,22 +1,15 @@
-// Copyright (c) 2017 Stefan Lankes, RWTH Aachen University
-//
-// Licensed under the Apache License, Version 2.0, <LICENSE-APACHE or
-// http://apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT or
-// http://opensource.org/licenses/MIT>, at your option. This file may not be
-// copied, modified, or distributed except according to those terms.
-
 // Implementations for x86_64.
-#[cfg(target_arch = "x86_64")]
-pub mod x86_64;
+#[cfg(any(target_arch = "x86_64", target_arch = "x86"))]
+pub mod x86;
 
 // Export our platform-specific modules.
 #[cfg(target_arch = "x86_64")]
 pub use self::x86_64::kernel::{irq, processor, serial};
 
 // Export our platform-specific modules.
-#[cfg(target_arch = "x86_64")]
-pub use self::x86_64::kernel::switch::switch;
+#[cfg(any(target_arch = "x86_64", target_arch = "x86"))]
+pub use self::x86::kernel::switch::switch;
 
 // Export our platform-specific modules.
-#[cfg(target_arch = "x86_64")]
-pub use self::x86_64::mm;
+#[cfg(any(target_arch = "x86_64", target_arch = "x86"))]
+pub use self::x86::mm;
