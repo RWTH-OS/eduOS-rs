587e6103f0899fac11a321502ded3f8a86f2b6f6
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2024-11-16 09:16:44 +0100

    initialize bss section on i686

--- src/arch/x86/mm/mod.rs
+++ src/arch/x86/mm/mod.rs
@@ -126,9 +126,28 @@ pub fn init() {
 }
 
 #[cfg(target_arch = "x86")]
+#[repr(C, align(64))]
+pub(crate) struct Aligned<T>(T);
+
+#[cfg(target_arch = "x86")]
+impl<T> Aligned<T> {
+	/// Constructor.
+	pub const fn new(t: T) -> Self {
+		Self(t)
+	}
+}
+
+#[cfg(target_arch = "x86")]
 extern "C" {
 	static BOOT_STACK: usize;
 }
+
+#[cfg(target_arch = "x86")]
+pub(crate) const BOOT_STACK_SIZE: usize = 0x10000;
+#[cfg(target_arch = "x86")]
+#[link_section = ".data"]
+pub(crate) static mut BOOT_STACK: Aligned<[u8; BOOT_STACK_SIZE]> =
+	Aligned::new([0; BOOT_STACK_SIZE]);
 
 #[cfg(target_arch = "x86")]
 pub(crate) fn get_boot_stack() -> BootStack {
