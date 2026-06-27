0df7bc403e5608a06e69a2da89517a6f827be334
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2026-06-27 10:34:35 +0200

    remove i686 support
    Drop the 32-bit x86 (i686) target entirely. x86_64, aarch64 and riscv64
    remain supported.
    
    - delete i686-eduos.json, entry32.s and link_i686.ld
    - remove the i686 target from .cargo/config.toml and the
      cfg(target_arch = "x86") dependency from Cargo.toml
    - drop the 32-bit code paths in arch/x86 (start, switch, task, mm) and
      simplify the now redundant any(x86_64, x86) cfg guards to x86_64
    - CI: remove the i686 build/clippy steps and the release job, which only
      published the i686 build
    - README: drop the i686 run instructions

--- src/arch/x86/kernel/mod.rs
+++ src/arch/x86/kernel/mod.rs
@@ -7,13 +7,8 @@ pub(crate) mod task;
 #[cfg(feature = "vga")]
 pub(crate) mod vga;
 
-#[cfg(target_arch = "x86_64")]
 use bootloader::BootInfo;
-#[cfg(target_arch = "x86_64")]
 pub(crate) static mut BOOT_INFO: Option<&'static BootInfo> = None;
-
-#[cfg(target_arch = "x86")]
-core::arch::global_asm!(include_str!("entry32.s"));
 
 pub(crate) fn init() {
 	processor::cpu_init();
