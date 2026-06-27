0243a7a3854234ba80deb553103fc33db7e9a9f0
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2026-06-27 10:41:37 +0200

    remove i686 support
    Drop the 32-bit x86 target (sources, target spec, build config, CI and
    docs); x86_64, aarch64 and riscv64 remain supported.

--- src/arch/x86/mm/mod.rs
+++ src/arch/x86/mm/mod.rs
@@ -1,8 +1,5 @@
 use crate::scheduler::task::Stack;
 
-#[cfg(target_arch = "x86")]
-pub use x86::bits32::paging::VAddr as VirtAddr;
-#[cfg(target_arch = "x86_64")]
 pub use x86::bits64::paging::VAddr as VirtAddr;
 
 #[allow(dead_code)]
@@ -20,13 +17,7 @@ impl BootStack {
 
 impl Stack for BootStack {
 	fn top(&self) -> VirtAddr {
-		cfg_if::cfg_if! {
-			if #[cfg(target_arch = "x86")] {
-				self.end - 16u32
-			} else {
-				self.end - 16u64
-			}
-		}
+		self.end - 16u64
 	}
 
 	fn bottom(&self) -> VirtAddr {
@@ -34,7 +25,6 @@ impl Stack for BootStack {
 	}
 }
 
-#[cfg(target_arch = "x86_64")]
 pub(crate) fn get_boot_stack() -> BootStack {
 	use crate::arch::x86::kernel::BOOT_INFO;
 	use bootloader::bootinfo::MemoryRegionType;
@@ -54,37 +44,4 @@ pub(crate) fn get_boot_stack() -> BootStack {
 
 		panic!("Unable to determine the kernel stack");
 	}
-}
-
-#[cfg(target_arch = "x86")]
-#[repr(C, align(64))]
-pub(crate) struct Aligned<T>(T);
-
-#[cfg(target_arch = "x86")]
-impl<T> Aligned<T> {
-	/// Constructor.
-	pub const fn new(t: T) -> Self {
-		Self(t)
-	}
-}
-
-#[cfg(target_arch = "x86")]
-pub(crate) const BOOT_STACK_SIZE: usize = 0x3000;
-#[cfg(target_arch = "x86")]
-#[link_section = ".data"]
-pub(crate) static mut BOOT_STACK: Aligned<[u8; BOOT_STACK_SIZE]> =
-	Aligned::new([0; BOOT_STACK_SIZE]);
-
-#[cfg(target_arch = "x86")]
-pub(crate) fn get_boot_stack() -> BootStack {
-	BootStack::new(
-		unsafe { VirtAddr((BOOT_STACK.0.as_ptr() as usize).try_into().unwrap()) },
-		unsafe {
-			VirtAddr(
-				(BOOT_STACK.0.as_ptr() as usize + BOOT_STACK_SIZE)
-					.try_into()
-					.unwrap(),
-			)
-		},
-	)
 }
--- src/consts.rs
+++ src/consts.rs
@@ -7,10 +7,6 @@
 pub(crate) const STACK_SIZE: usize = 0x3000;
 
 /// Define the size of the kernel stack
-#[cfg(target_arch = "x86")]
-pub(crate) const STACK_SIZE: usize = 0x2000;
-
-/// Define the size of the kernel stack
 #[cfg(target_arch = "aarch64")]
 pub(crate) const STACK_SIZE: usize = 0x3000;
 
@@ -22,7 +18,7 @@ pub(crate) const STACK_SIZE: usize = 0x3000;
 pub(crate) const CACHE_LINE: usize = 64;
 
 /// Size of a page frame on a x86_64 processor
-#[cfg(any(target_arch = "x86_64", target_arch = "x86"))]
+#[cfg(target_arch = "x86_64")]
 pub(crate) const PAGE_SIZE: usize = 4096;
 
 /// Size of the kernel heap
